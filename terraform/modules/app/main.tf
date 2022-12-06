ac0b3227696898cda07917783cdb93866061ad2a
Vasiliy Basov <vasiliy.basov.82@gmail.com>
2022-12-06 22:28:26 +0300

    Добавлено http-server тег

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "app" {
   name         = "reddit-app-${var.prod_or_stage}"
   machine_type = "g1-small"
   zone         = var.zone
-  tags         = ["reddit-app"] # можем определить теги
+  tags         = ["reddit-app", "http-server"] # можем определить теги если определяем тег http-server то gcp автоматически открывает порт 80
   labels = {
     ansible_group = "app"  # можем определить labels
   }
@@ -98,9 +98,9 @@ resource "google_compute_instance" "app_without_provisioning" {
   name         = "reddit-app-${var.prod_or_stage}"
   machine_type = "g1-small"
   zone         = var.zone
-  tags         = ["reddit-app"] # можем определить теги
+  tags         = ["reddit-app", "http-server"] # можем определить теги если определяем тег http-server то gcp автоматически открывает порт 80
   labels       = {
-    ansible_group = "app"  # можем определить labels
+    ansible_group = "app"  # можем определить labels, определяем для того чтобы собирать dynamic inventory в ansible через переменную ansible_group
   }
 
   # определение загрузочного диска
